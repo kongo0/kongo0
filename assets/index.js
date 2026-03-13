066750edc114ee12df23098db5052f68a7672536
kongo <78408422+kongo0@users.noreply.github.com>
2026-03-13 17:44:10 +0100

    Refactor image upload logic to use FileReader
    Updated image input handling to use FileReader for local image preview.

--- assets/index.js
+++ assets/index.js
@@ -23,7 +23,7 @@ var upload = document.querySelector(".upload");
 
 var imageInput = document.createElement("input");
 imageInput.type = "file";
-imageInput.accept = ".jpeg,.png,.jpg,.gif";
+imageInput.accept = "image/*";
 
 document.querySelectorAll(".input_holder").forEach((element) => {
   var input = element.querySelector(".input");
@@ -40,8 +40,7 @@ upload.addEventListener("click", () => {
 
 imageInput.addEventListener("change", () => {
 
-  var file = imageInput.files[0];
-
+  const file = imageInput.files[0];
   if (!file) return;
 
   if (file.size > 5000000) {
@@ -52,121 +51,29 @@ imageInput.addEventListener("change", () => {
   upload.classList.remove("upload_loaded");
   upload.classList.add("upload_loading");
 
-  upload.removeAttribute("selected");
+  const reader = new FileReader();
 
-  var data = new FormData();
-  data.append("image", file);
+  reader.onload = function(e) {
 
-  fetch("https://api.imgur.com/3/image", {
-    method: "POST",
-    headers: {
-      Authorization: "Client-ID e4d98a899c8c946"
-    },
-    body: data
-  })
-  .then(res => res.json())
-  .then(response => {
-
-    if (!response.success) {
-      throw new Error("Upload failed");
-    }
-
-    var url = response.data.link;
-
-    upload.classList.remove("error_shown");
-    upload.classList.remove("upload_loading");
-    upload.classList.add("upload_loaded");
+    const url = e.target.result;
 
     upload.setAttribute("selected", url);
 
-    var img = upload.querySelector(".upload_uploaded");
+    upload.classList.remove("upload_loading");
+    upload.classList.add("upload_loaded");
+
+    const img = upload.querySelector(".upload_uploaded");
 
     if (img) {
       img.src = url;
     }
 
-  })
-  .catch(err => {
+  };
 
-    console.error(err);
+  reader.readAsDataURL(file);
 
-    upload.classList.remove("upload_loading");
-    upload.classList.add("error_shown");
-
-    alert("Błąd podczas wgrywania zdjęcia.");
-  });
 });
 
 document.querySelector(".go").addEventListener("click", () => {
 
-  var empty = [];
-  var params = new URLSearchParams();
-
-  params.set("sex", sex);
-
-  if (!upload.hasAttribute("selected")) {
-    empty.push(upload);
-    upload.classList.add("error_shown");
-  } else {
-    params.set("image", upload.getAttribute("selected"));
-  }
-
-  const day = document.getElementById("day");
-  const month = document.getElementById("month");
-  const year = document.getElementById("year");
-
-  [day, month, year].forEach((input) => {
-
-    if (isEmpty(input.value)) {
-      empty.push(input);
-      input.classList.add("error_shown");
-    } else {
-      params.set(input.id, input.value);
-    }
-
-  });
-
-  document.querySelectorAll(".input_holder").forEach((element) => {
-
-    var input = element.querySelector(".input");
-
-    if (isEmpty(input.value)) {
-
-      empty.push(element);
-      element.classList.add("error_shown");
-
-    } else {
-
-      params.set(input.id, input.value);
-
-    }
-
-  });
-
-  if (empty.length !== 0) {
-
-    empty[0].scrollIntoView({
-      behavior: "smooth"
-    });
-
-  } else {
-
-    forwardToId(params);
-
-  }
-
-});
-
-function isEmpty(value) {
-  return /^\s*$/.test(value);
-}
-
-function forwardToId(params) {
-  location.href = "id.html?" + params.toString();
-}
-
-var guide = document.querySelector(".guide_holder");
-
-guide.addEventListener("click", () => {
-  guide.classList.toggle("unfolded");
-});
+  var

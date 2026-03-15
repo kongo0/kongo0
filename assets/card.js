ea73484da586628d1e89a39682770e3cadc510b1
kongo <78408422+kongo0@users.noreply.github.com>
2026-03-15 10:37:50 +0100

    Refactor card.js for better data handling and updates

--- assets/card.js
+++ assets/card.js
@@ -1,232 +1,117 @@
-var confirmElement = document.querySelector(".confirm");
-var time = document.getElementById("time");
-
-/* ===== DATA AKTUALIZACJI ===== */
-
-if (localStorage.getItem("update") == null) {
-  localStorage.setItem("update", "24.12.2024");
-}
-
-var date = new Date();
-
-var updateText = document.querySelector(".bottom_update_value");
-if(updateText){
-updateText.innerHTML = localStorage.getItem("update");
-}
-
-var update = document.querySelector(".update");
-
-if(update){
-update.addEventListener("click", () => {
-
-  var newDate = date.toLocaleDateString("pl-PL");
-
-  localStorage.setItem("update", newDate);
-  updateText.innerHTML = newDate;
-
-  scroll(0,0);
-
-});
-}
+var params = new URLSearchParams(window.location.search);
 
 /* ===== ZEGAR ===== */
 
-setClock();
-
 function setClock(){
-
-  date = new Date();
+  var date = new Date();
+  var time = document.getElementById("time");
 
   if(time){
-  time.innerHTML =
-  "Czas: " +
-  date.toLocaleTimeString("pl-PL") +
-  " " +
-  date.toLocaleDateString("pl-PL");
+    time.innerHTML =
+      date.toLocaleTimeString("pl-PL") + " " +
+      date.toLocaleDateString("pl-PL");
   }
 
   setTimeout(setClock,1000);
-
 }
 
-/* ===== ROZWIJANIE DODATKOWYCH DANYCH ===== */
-
-var unfold = document.querySelector(".info_holder");
-
-if(unfold){
-unfold.addEventListener("click",()=>{
-
-  if(unfold.classList.contains("unfolded")){
-  unfold.classList.remove("unfolded");
-  }
-  else{
-  unfold.classList.add("unfolded");
-  }
-
-});
-}
-
-/* ===== POBIERANIE DANYCH Z LINKU ===== */
-
-var params = new URLSearchParams(window.location.search);
-
-function loadReadyData(result){
-
-Object.keys(result).forEach((key)=>{
-result[key]=htmlEncode(result[key]);
-});
-
-const birthdayDate = new Date();
-
-birthdayDate.setFullYear(result["year"], result["month"]-1, result["day"]);
-
-var sex = result["sex"];
-
-let day = birthdayDate.getDate();
-let month = birthdayDate.getMonth()+1;
-let year = birthdayDate.getFullYear();
-
-var textSex;
-
-if(sex==="m"){
-textSex="Mężczyzna";
-}else{
-textSex="Kobieta";
-}
-
-/* ===== SERIA I NUMER ===== */
-
-var seriesAndNumber = localStorage.getItem("seriesAndNumber");
-
-if(!seriesAndNumber){
-
-seriesAndNumber="";
-
-var chars="ABCDEFGHIJKLMNOPQRSTUWXYZ".split("");
-
-for(var i=0;i<4;i++){
-seriesAndNumber+=chars[Math.floor(Math.random()*chars.length)];
-}
-
-seriesAndNumber+=" ";
-
-for(var i=0;i<5;i++){
-seriesAndNumber+=Math.floor(Math.random()*9);
-}
-
-localStorage.setItem("seriesAndNumber",seriesAndNumber);
-
-}
-
-day = day>9 ? day : "0"+day;
-month = month>9 ? month : "0"+month;
-
-setData("seriesAndNumber",seriesAndNumber);
-setData("name",result["name"].toUpperCase());
-setData("surname",result["surname"].toUpperCase());
-setData("nationality",result["nationality"].toUpperCase());
-
-setData("fathersName","WOJCIECH");
-setData("mothersName","AGATA");
-
-setData("birthday",day+"."+month+"."+year);
-
-setData("familyName",result["familyName"]);
-setData("sex",textSex);
-
-setData("fathersFamilyName",result["fathersFamilyName"]);
-setData("mothersFamilyName",result["mothersFamilyName"]);
-
-setData("birthPlace",result["birthPlace"]);
-setData("countryOfBirth",result["countryOfBirth"]);
-
-setData(
-"adress",
-"ul. "+
-result["address1"]+
-"<br>"+
-result["address2"]+
-" "+
-result["city"]
-);
-
-/* ===== DATY ===== */
-
-var givenDate = new Date(birthdayDate);
-givenDate.setFullYear(givenDate.getFullYear()+18);
-
-setData("givenDate",givenDate.toLocaleDateString("pl-PL"));
-
-var expiryDate = new Date(givenDate);
-expiryDate.setFullYear(expiryDate.getFullYear()+10);
-
-setData("expiryDate",expiryDate.toLocaleDateString("pl-PL"));
-
-/* ===== PESEL ===== */
-
-if(parseInt(year)>=2000){
-month=20+parseInt(month);
-}
-
-var later;
-
-if(sex==="m"){
-later="0295";
-}else{
-later="0382";
-}
-
-if(day<10) day="0"+day;
-if(month<10) month="0"+month;
-
-var pesel = year.toString().substring(2)+month+day+later+"7";
-
-setData("pesel",pesel);
-
-}
-
-/* ===== ZAŁADOWANIE DANYCH ===== */
-
-let result = Object.fromEntries(params);
-
-if(Object.keys(result).length>0){
-loadReadyData(result);
-}
-
-/* ===== ŁADOWANIE ZDJĘCIA (NAPRAWIONE) ===== */
-
-function loadImage(){
-
-var image = localStorage.getItem("image");
-
-if(image){
-setImage(image);
-}
-
-}
-
-function setImage(image){
-
-var img = document.querySelector(".id_own_image");
-
-if(img){
-img.style.backgroundImage = "url('"+image+"')";
-img.style.backgroundSize = "cover";
-img.style.backgroundPosition = "center";
-}
-
-}
-
-loadImage();
+setClock();
 
 /* ===== USTAWIANIE DANYCH ===== */
 
 function setData(id,value){
-
-var el = document.getElementById(id);
-
-if(el){
-el.innerHTML=value;
+  var el = document.getElementById(id);
+  if(el){
+    el.innerHTML = value;
+  }
 }
 
+/* ===== ŁADOWANIE DANYCH Z GENERATORA ===== */
+
+function loadReadyData(result){
+
+  const birthdayDate = new Date();
+  birthdayDate.setFullYear(result["year"], result["month"]-1, result["day"]);
+
+  let day = birthdayDate.getDate();
+  let month = birthdayDate.getMonth()+1;
+  let year = birthdayDate.getFullYear();
+
+  day = day>9 ? day : "0"+day;
+  month = month>9 ? month : "0"+month;
+
+  setData("name", result["name"]?.toUpperCase());
+  setData("surname", result["surname"]?.toUpperCase());
+  setData("nationality", result["nationality"]?.toUpperCase());
+  setData("birthday", day+"."+month+"."+year);
+
+  var pesel = year.toString().substring(2)+month+day+"12345";
+  setData("pesel", pesel);
+}
+
+/* ===== PARAMETRY Z LINKU ===== */
+
+let result = Object.fromEntries(params);
+
+if(Object.keys(result).length>0){
+  loadReadyData(result);
+}
+
+/* ===== ŁADOWANIE ZDJĘCIA (NAPRAWA) ===== */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  var image = localStorage.getItem("image");
+
+  if(image){
+
+    var container = document.querySelector(".id_own_image");
+
+    if(container){
+
+      container.innerHTML = "";
+
+      var img = document.createElement("img");
+      img.src = image;
+
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.objectFit = "cover";
+
+      container.appendChild(img);
+
+    }
+
+  }
+
+});
+
+/* ===== AKTUALIZACJA ===== */
+
+var update = document.querySelector(".update");
+
+if(update){
+
+  update.addEventListener("click", ()=>{
+
+    var date = new Date().toLocaleDateString("pl-PL");
+
+    localStorage.setItem("update",date);
+
+    var el = document.querySelector(".bottom_update_value");
+    if(el){
+      el.innerHTML = date;
+    }
+
+  });
+
+}
+
+var last = localStorage.getItem("update");
+
+if(last){
+  var el = document.querySelector(".bottom_update_value");
+  if(el){
+    el.innerHTML = last;
+  }
 }
